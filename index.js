8b4c5be58bab190d5e1a0f7cc2c9d983db7e6dd2
yehudit12 <y7173245@gmail.com>
2023-05-10 21:04:56 +0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const express = require('express');
+const app=express()
 
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
-append.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
     sdk.auth('rnd_S0EzCiZc6fy4eekXwHOBqmDZGGs1');
     sdk.getServices({limit: '20'})
-      .then(({ data }) => {console.log(data);res.json(data)})
+      .then(({ data }) => {console.log(data);res.send(data)})
       .catch(err => console.error(err));
 })
-
+app.listen(3000,()=>{console.log("kiuy")})
